63032f984e5dd9b13ff58f9da889206f8a122c4b
Fariza Akhmetova <ahmetova.fariza@gmail.com>
2024-08-16 20:45:40 +0300

    add logout button

--- src/components/panel/partials/PanelNavbar.js
+++ src/components/panel/partials/PanelNavbar.js
@@ -3,22 +3,33 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const PanelNavbar = () => {
+  const cikisYap = () => {};
   return (
     <PanelNavContainer>
       <MyLink to='dersler'>Dersler</MyLink>
       <MyLink to='canli-dersler'>Canlı dersler</MyLink>
       <MyLink to='mesajlar'>Mesajlar</MyLink>
       <MyLink to='notlar'>Notlar</MyLink>
+      <Button onClick={cikisYap}>Çıkış Yap</Button>
     </PanelNavContainer>
   );
 };
 const PanelNavContainer = styled.div`
   display: flex;
   justify-content: space-around;
+  align-items: center;
   background-color: #aeb6bf;
   padding: 2rem;
 `;
-
+const Button = styled.button`
+  border: none;
+  border-radius: 7px;
+  background-color: #283747;
+  color: #fff;
+  font-weight: 700;
+  padding: 10px 20px;
+  font-size: 1.2rem;
+`;
 const MyLink = styled(Link)`
   color: #283747;
   font-weight: 800;
