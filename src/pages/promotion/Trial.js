7aa8d5ef7124205f216e8d93ae187f35f1f418f0
Fariza Akhmetova <ahmetova.fariza@gmail.com>
2025-01-03 20:04:47 +0300

    refactoring

--- src/pages/promotion/Trial.js
+++ src/pages/promotion/Trial.js
@@ -7,103 +7,142 @@ const Trial = () => {
       id: 1,
       title: '1. Internet, browser, domain tanımları',
       source: 'https://www.youtube.com/embed/NErrGZ64OdE?si=OXAsmHAkCvbNRcB_',
+      description:
+        ' Internet. Browser.Domain. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 2,
       title: '2. HTML5 konseptleri',
       source: 'https://www.youtube.com/embed/TxZwqVTaCmA?si=UwOZF9ca4tW-0ea8',
+      description:
+        ' HTML CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 3,
       title: '3. CSS3 ana kavramları',
       source: 'https://www.youtube.com/embed/SqcY0GlETPk?si=LAYRCLWf6z6_0PiC',
+      description:
+        'CSS CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 4,
       title: '4. SASS avantajları',
       source: 'https://www.youtube.com/embed/_uQrJ0TkZlc?si=G3JCL-9OwkRqqPrg ',
+      description:
+        'SASS CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 5,
       title: '5. GitHub önemi',
       source: 'https://www.youtube.com/embed/k9WqpQp8VSU?si=U2sbDJzWF3VKnZWe',
+      description:
+        'GITHUB CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 6,
       title: '6. JavaScript(başlangıç seviyesi)',
       source: 'https://www.youtube.com/embed/NpUuuT_EzSs?si=Ch_RZma46QuX3iOE',
+      description:
+        'JAVASCRIPT CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 7,
       title: '7. JavaScript(ileri seviyesi)',
       source: 'https://www.youtube.com/embed/lsMQRaeKNDk?si=rFEnqPtGgiUSDxtT',
+      description:
+        'JAVASCRIPT - ADVANCED LEVEL. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 8,
       title: '8. REST API nedir',
       source: 'https://www.youtube.com/embed/2mqN7ZhDsUA?si=seWCfU47G0arbadJ',
+      description:
+        'API CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 9,
       title: '9. React.js nedir',
       source: 'https://www.youtube.com/embed/pBSch5AsJWs?si=oAxXFWleYgWBu43Y',
+      description:
+        'REACT CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 10,
       title: '10. CSS3 ana kavramları',
       source: 'https://www.youtube.com/embed/NErrGZ64OdE?si=OXAsmHAkCvbNRcB_',
+      description:
+        'CSS MAIN CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 11,
       title: '11. Bootstrap avantajları',
       source: 'https://www.youtube.com/embed/TxZwqVTaCmA?si=UwOZF9ca4tW-0ea8',
+      description:
+        'BOOTSTRAP CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 12,
       title: '12. Webpack önemi',
       source: 'https://www.youtube.com/embed/SqcY0GlETPk?si=LAYRCLWf6z6_0PiC',
+      description:
+        'WEBPACK CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 13,
       title: '13. Python(başlangıç seviyesi)',
       source: 'https://www.youtube.com/embed/_uQrJ0TkZlc?si=G3JCL-9OwkRqqPrg ',
+      description:
+        'PYTHON CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 14,
       title: '14. Python(ileri seviyesi)',
       source: 'https://www.youtube.com/embed/k9WqpQp8VSU?si=U2sbDJzWF3VKnZWe',
+      description:
+        'PYTHON ADVANCED CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 15,
       title: '15. Node.js nedir',
       source: 'https://www.youtube.com/embed/NpUuuT_EzSs?si=Ch_RZma46QuX3iOE',
+      description:
+        'NODE.JS CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 16,
       title: '16. Redux Toolkit nedir',
       source: 'https://www.youtube.com/embed/lsMQRaeKNDk?si=rFEnqPtGgiUSDxtT',
+      description:
+        'REDUX TOOLKIT CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 17,
       title: '17. SQL nedir',
       source: 'https://www.youtube.com/embed/2mqN7ZhDsUA?si=seWCfU47G0arbadJ',
+      description:
+        'SQL CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
     {
       id: 18,
       title: '18. Tailwind CSS nedir',
       source: 'https://www.youtube.com/embed/pBSch5AsJWs?si=oAxXFWleYgWBu43Y',
+      description:
+        'TAILWIND CONCEPTS. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.',
     },
   ];
-  const videos = [
-    {
-      id: 14,
-    },
-  ];
-  const [lessonId, setLessonId] = useState(1);
-  const [source, setSource] = useState('');
+
+  const [video, setVideo] = useState(
+    'https://www.youtube.com/embed/NErrGZ64OdE?si=OXAsmHAkCvbNRcB_'
+  );
+  const [title, setTitle] = useState('1. Internet, browser, domain tanımları');
+  const [desc, setDesc] = useState(
+    ' Internet. Browser.Domain. Vestibulum lacus ligula sollicitudin ultrices parturient nunc.'
+  );
   const lessonHandler = (id) => {
-    setLessonId(id);
-    setSource(id);
+    let kurs = lessons.find((data) => data.id === id);
+    setVideo(kurs.source);
+    setTitle(kurs.title);
+    setDesc(kurs.description);
   };
   return (
     <TrialSection>
@@ -115,40 +154,19 @@ const Trial = () => {
       <TrialHeader>Deneme dersleri izleyip karar verin!</TrialHeader>
       <TrialWrapper>
         <TrialVideoWrapper>
-          {lessons.map((el) =>
-            el.id === lessonId ? (
-              <>
-                <TrialVideo
-                  key={el.id}
-                  width='560'
-                  height='315'
-                  src={el.source}
-                  title='YouTube video player'
-                  frameborder='0'
-                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                  referrerpolicy='strict-origin-when-cross-origin'
-                  allowfullscreen></TrialVideo>
-                <TrialTitle key={el.id}>{el.title}</TrialTitle>
-                <TrialText>
-                  Scelerisque inceptos porttitor semper bibendum sagittis felis.
-                  Massa aliquet eu ridiculus, rutrum torquent dictum libero.
-                  Vestibulum lacus ligula sollicitudin ultrices parturient nunc.
-                  Maximus lectus facilisi felis morbi curabitur ad sollicitudin
-                  ridiculus. Dignissim tempor elementum tristique sem ad justo
-                  molestie. Fusce accumsan porttitor etiam ad nisi ante etiam.
-                </TrialText>
-              </>
-            ) : (
-              ''
-            )
-          )}
-          {/* {lessons.map((el) =>
-            el.id === titleId ? (
-              
-            ) : (
-              ''
-            )
-          )} */}
+          <>
+            <TrialVideo
+              width='560'
+              height='315'
+              src={video}
+              title='YouTube video player'
+              frameborder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              referrerpolicy='strict-origin-when-cross-origin'
+              allowfullscreen></TrialVideo>
+            <TrialTitle>{title}</TrialTitle>
+            <TrialText>{desc}</TrialText>
+          </>
         </TrialVideoWrapper>
 
         <TrialLessons>
